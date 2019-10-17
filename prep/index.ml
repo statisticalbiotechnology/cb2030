69fc7d823668c765abcb06daad6c00cf16b2017d
Lukas Kall <lukas.kall@scilifelab.se>
2019-10-17 17:06:47 +0200

    Trying again

---
+++ prep/index.ml
@@ -0,0 +1,17 @@
+# Jupyter Notebooks
+
+
+
+## On-line usage
+
+Most of the contained notebooks can be executed on-line, using services provided by mybinder.org. Just click the small icon present in the readme files of each subfolder, and wait for the notebook to be executed in an online docker container.
+
+## Download
+To use this material off-line follow the following steps:
+
+1. Install the anaconda development environment from https://www.anaconda.com/distribution/#download-section
+2. Use the links above to download the repository as a zipfile(, or better clone the repository using git).
+3. Place yourself in the newly created cb2030 folder
+4. Install the right environment by issuing `conda env create -f environment.yml`
+5. Activate your new environment using `conda activate p37`
+6. Run the command `jupyter-notebook-3.7 .`
