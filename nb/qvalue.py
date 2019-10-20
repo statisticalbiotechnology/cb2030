fb79a3ef5f03b3ba6424887b19664227440f6d99
Lukas Käll <lukas.kall@gmail.com>
2019-10-21 00:37:31 +0200

    got gseapy to run

---
+++ nb/qvalue.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+import numpy.random as npr
+
+def bootstrap(invec):
+    idx = npr.randint(0, len(invec), len(invec))
+    return [invec[i] for i in idx]
+
+def estimatePi0(p, numBoot=100, numLambda=100, maxLambda=0.95):
+    p.sort()
+    n=len(p)
+    lambdas=np.linspace(maxLambda/numLambda,maxLambda,numLambda)
+    Wls=np.array([n-np.argmax(p>=l) for l in lambdas])
+    pi0s=np.array([Wls[i] / (n * (1 - lambdas[i])) for i in range(numLambda)])
+    minPi0=np.min(pi0s)
+    mse = np.zeros(numLambda)
+    for boot in range(numBoot):
+        pBoot = bootstrap(p)
+        pBoot.sort()
+        WlsBoot =np.array([n-np.argmax(pBoot>=l) for l in lambdas])
+        pi0sBoot =np.array([WlsBoot[i] / (n *(1 - lambdas[i])) for i in range(numLambda)])
+        mse = mse + np.square(pi0sBoot-minPi0)
+    minIx = np.argmin(mse)
+    return pi0s[minIx]
+
+def qvalues(pvalues):
+    m = pvalues.shape[0] # The number of p-values
+    pvalues.sort_values("p",inplace=True) # sort the pvalues in acending order
+    pi0 = estimatePi0(list(pvalues["p"].values))
+    
+    # calculate a FDR(t) as in Storey & Tibshirani
+    num_p = 0.0
+    for ix in pvalues.index:
+        num_p += 1.0
+        fdr = pi0*pvalues.loc[ix,"p"]*m/num_p
+        pvalues.loc[ix,"q"] = fdr
+    
+    # calculate a q(p) as the minimal FDR(t)
+    old_q=1.0
+    for ix in reversed(list(pvalues.index)):
+        q = min(old_q,pvalues.loc[ix,"q"])
+        old_q = q
+        pvalues.loc[ix,"q"] = q
+    return pvalues
---
+++ nb/tcga_read.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import tarfile
+import requests
+
+def track_dl(url,tar):
+    response = requests.get(url, stream=True)
+    with open(tar, "wb") as handle:
+        for data in response.iter_content():
+            handle.write(data)
+
+def get_tar(url,path):
+    try:
+        tf = tarfile.open(path)
+    except:
+        track_dl(url, path)
+        tf = tarfile.open(path)
+    return tf
+
+def get_expression_data(path,url,file):
+    df = get_data(path,url,file)
+    df.dropna(axis=0, how='any', inplace=True)
+    df.set_index('Hugo_Symbol', inplace=True)
+    df.drop(columns=['Unnamed: 0', 'Entrez_Gene_Id'], inplace=True)
+    #df.drop(columns=['Entrez_Gene_Id'], inplace=True)
+    df = df.reindex(sorted(df.columns), axis=1)
+    return df
+
+def get_clinical_data(path,url,file):
+    df = get_data(path,url,file).T
+    df.columns = df.iloc[2]
+    df.drop(columns=["A unique sample identifier.","STRING","1","SAMPLE_ID"], inplace=True)
+    df.drop(index=["Unnamed: 0","#Patient Identifier","Sample Identifier","Other Sample ID"], inplace=True)
+    df = df.reindex(sorted(df.columns), axis=1)
+    return df
+
+def get_data(path,url,file):
+    try:
+        df = pd.read_csv(path, sep="\t")
+    except:
+        tf = get_tar(url,"my.tar.gz")
+        tf.extract(file)
+        df = pd.read_csv(file, sep="\t")
+        df.to_csv(path, sep="\t")
+    return df
